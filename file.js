1f2620d8fd2f43eb53af840dda1ed74d3b3a317a
BasilAhmedd <basil.0522002@gmail.com>
2024-05-26 12:48:00 +0300

    added slider

--- file.js
+++ file.js
@@ -2,3 +2,6 @@ function playSound(id) {
   var audio = document.getElementById(id);
   audio.play();
 }
+function slider(i){
+  var slide =document.querySelector(".bigdiv").style.marginLeft = `${-100 * i}vw`;
+}
